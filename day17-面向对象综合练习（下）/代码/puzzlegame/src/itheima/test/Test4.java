e31c9e878928b812f861b4a77f6d9c2c1845d2c1
shizheng <ZhengShiWork@outlook.com>
2023-05-06 14:44:53 +0800

    提交

--- day17-面向对象综合练习（下）/代码/puzzlegame/src/itheima/test/Test4.java
+++
@@ -1,7 +0,0 @@
-package itheima.test;
-
-public class Test4 {
-    public static void main(String[] args) {
-        new MyJFrame3();
-    }
-}
--- day18-API（常见API，对象克隆）/资料/课堂练习Runtime的素材/Test.java
+++
@@ -1,7 +0,0 @@
-package com.itheima.a03runtimedemo;
-
-public class Test {
-    public static void main(String[] args) {
-        new MyJframe();
-    }
-}
